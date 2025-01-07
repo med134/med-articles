f8d8c310f7031b9eed4b0a88f42a90e52c25401f
med134 <81263260+med134@users.noreply.github.com>
2025-01-07 20:51:26 +0100

    add7

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -1,6 +1,45 @@
-"use server"
-import { signIn } from "@/auth";
+"use server";
+import { auth, signIn,signOut } from "@/auth";
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
+import { connect } from "./ConnectDB";
+import User from "../modalMongodb/User";
 
 export const handelLoginGithub = async () => {
-    await signIn("github");
-  };
+  await signIn("github");
+};
+export const handelLogOut = async () => {
+  try {
+    await signOut();
+  } catch (err) {
+    console.log(err);
+  }
+  revalidatePath("/dashboard");
+  redirect("/login");
+};
+export const getUserId = async () => {
+  const session = await auth();
+  if (session) {
+    const email = session?.user?.email;
+    if (email) {
+      const user = await getUserByEmail(email);
+      return JSON.parse(JSON.stringify(user));
+    }
+  } else {
+    return null;
+  }
+};
+
+export const getUserByEmail = async (email: string) => {
+  if (email) {
+    try {
+      connect();
+      const result = await User.findOne({ email });
+      const user = JSON.parse(JSON.stringify(result));
+      return user;
+    } catch (err) {
+      console.log(err);
+      throw new Error("Failed to fetch users!");
+    }
+  }
+};

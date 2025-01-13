9795f0481aee747a8ccdf6bc8305a5f5a6dc218a
med134 <81263260+med134@users.noreply.github.com>
2025-01-13 23:32:44 +0100

    ad

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -5,14 +5,19 @@ import React from "react";
 import { Suspense } from "react";
 import SkeltonCard from "../components/SkeltonCard";
 import ListAllBlogs from "../components/ListAllBlogs";
-
+interface Category {
+  _id: string;
+  label: string;
+  value: string;
+  image: string;
+}
 const page = async () => {
   const categories = await getAllCategories();
   const posts = await getArticleByCategories("all");
   return (
     <div className="bg-light dark:bg-dark w-full py-32 p-6 sm:p-2 sm:py-16">
       <div className="flex justify-around items-center px-16 pt-16 lg:px-4 md:flex-wrap sm:px-4">
-        {categories?.map((item) => (
+        {categories?.map((item: Category) => (
           <Link
             className={`text-sm md:mb-3 w-24 flex flex-col justify-center items-center h-16 xl:h-10 sm:ml-2 dark:text-light sm:px-20 sm:rounded-md xs:w-24 sm:h-6 xs:bg-mainColor xs:text-light `}
             key={item._id}
